48c3def44a13a40720b82a6034597e1d7dc7670e
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-06 19:44:20 +0300

    добавление логики запуска сервера

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/FooxyS/auth-service/router"
+)
+
+func main() {
+	router := router.SetupRouter()
+	errStart := http.ListenAndServe("localhost:8080", router)
+	if errStart != nil {
+		log.Fatalf("error with starting the server: %v", errStart)
+	}
+}
