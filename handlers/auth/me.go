487dd966008429472fce49cc2cf6ae733d78c58d
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-07 11:43:57 +0300

    написание логики отправки uuid через json

--- handlers/auth/me.go
+++ handlers/auth/me.go
@@ -1,7 +1,36 @@
 package auth
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type UserJsonID struct {
+	UserID string `json:"userid"`
+}
 
 func MeHandler(w http.ResponseWriter, r *http.Request) {
+	type UserIDkey string
+	const ctxValueKey UserIDkey = "UserIDKey"
 
+	id, ok := r.Context().Value(ctxValueKey).(string)
+	if !ok || id == "" {
+		log.Println("wrong type or empty context")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	jsonresp := UserJsonID{
+		UserID: id,
+	}
+	errParseJson := json.NewEncoder(w).Encode(jsonresp)
+	if errParseJson != nil {
+		log.Printf("error with parsing json response: %v\n", errParseJson)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
