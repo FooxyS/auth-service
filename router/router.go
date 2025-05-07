a3918dbc63036d47b140d29b966cb62433b8da30
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-07 20:10:57 +0300

    обернул /auth/me в middleware

--- router/router.go
+++ router/router.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 
 	auth "github.com/FooxyS/auth-service/handlers/auth"
+
+	"github.com/FooxyS/auth-service/middleware"
 )
 
 func SetupRouter() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/auth/init", auth.InitHandler)
 	mux.HandleFunc("/auth/logout", auth.LogoutHandler)
-	mux.HandleFunc("/auth/me", auth.MeHandler)
+	mux.Handle("/auth/me", middleware.AuthMiddleware(http.HandlerFunc(auth.MeHandler)))
 	mux.HandleFunc("/auth/refresh", auth.RefreshHandler)
 	return mux
 }
