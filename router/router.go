f6b74b8932b7e1bbc455b6ce3014b9afc23a529f
Тима <timofeykolotovkin2@gmail.com>
2025-05-09 17:15:07 +0300

    добавил передачу пула соединений в роутер

--- router/router.go
+++ router/router.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 
 	auth "github.com/FooxyS/auth-service/handlers/auth"
+	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/FooxyS/auth-service/middleware"
 )
 
-func SetupRouter() http.Handler {
+func SetupRouter(db *pgxpool.Pool) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/auth/init", auth.InitHandler)
 	mux.HandleFunc("/auth/logout", auth.LogoutHandler)
 	mux.Handle("/auth/me", middleware.AuthMiddleware(http.HandlerFunc(auth.MeHandler)))
 	mux.HandleFunc("/auth/refresh", auth.RefreshHandler)
-	return mux
+	wrappedmux := middleware.PostgresWithContext(db, mux)
+
+	return wrappedmux
 }
