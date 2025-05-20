0f9e280b6c848d98b84cf884e057a1f37797dfe6
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-20 17:51:10 +0300

    feat: created new file with functions for register

---
+++ auth/services/register_service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/FooxyS/auth-service/auth/apperrors"
+	"github.com/FooxyS/auth-service/auth/models"
+	"github.com/jackc/pgx/v5"
+)
+
+func CompareRequestMethod(r *http.Request, method string) error {
+	if r.Method != method {
+		return apperrors.ErrMethodNotSupport
+	}
+	return nil
+}
+
+// координирует процесс регистрации пользователя
+func ProccessUser(ctx context.Context, ud models.NewUser, db models.DBClient) error {
+	//валидируем данные пользователя
+	errEmpty := ud.Validate()
+	if errEmpty != nil {
+		return errEmpty
+	}
+
+	//проверяем есть ли он в базе
+	_, errFind := db.FindUser(ctx, ud.GetEmail())
+
+	if errFind == nil {
+		return apperrors.ErrUserExist
+	}
+
+	if !errors.Is(errFind, pgx.ErrNoRows) {
+		return fmt.Errorf("error with FindUser: %v", errFind)
+	}
+
+	//записываем в базу
+	errWithSave := db.SaveUser(ctx, ud.Save())
+	if errWithSave != nil {
+		return fmt.Errorf("error with SaveUser: %v", errWithSave)
+	}
+
+	return nil
+}
