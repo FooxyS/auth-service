e215c77faf1caff8845148f5f198f83417a061df
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-20 17:46:13 +0300

    added new custom errors for register

--- auth/apperrors/errors.go
+++ auth/apperrors/errors.go
@@ -3,7 +3,11 @@ package apperrors
 import "errors"
 
 var (
-	ErrEmptyString  = errors.New("string is empty")
-	ErrNotMatch     = errors.New("this variables don't equael")
-	ErrInvalidToken = errors.New("invalid Token")
+	ErrEmptyString      = errors.New("string is empty")
+	ErrNotMatch         = errors.New("this variables don't equael")
+	ErrInvalidToken     = errors.New("invalid Token")
+	ErrMethodNotSupport = errors.New("request has unsupported method")
+	ErrSaveUser         = errors.New("error with saving user to db")
+	ErrUserExist        = errors.New("user already exists")
+	ErrEmptyField       = errors.New("request data has empty fields")
 )
