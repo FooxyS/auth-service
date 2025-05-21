b4dcbc70a360fd251e53ee5ae34ba103cd00ddb5
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-21 17:02:47 +0300

    refactor: методы интерфейсов
    добавил поиск по pairId в SessionRepository, убрал CheckPair (сделал напрямую в usecase), обновил название

--- internal/domain/interfaces.go
+++ internal/domain/interfaces.go
@@ -11,15 +11,15 @@ type UserRepository interface {
 type SessionRepository interface {
 	Save(ctx context.Context, session Session) error
 	Delete(ctx context.Context, id string) error
-	UpdateToken(ctx context.Context, pair, refreshHash string) error
+	UpdateSession(ctx context.Context, oldPair, pair, refreshHash string) error
+	FindByPairID(ctx context.Context, pairID string) (Session, error)
 }
 
 type TokenService interface {
 	GenerateAccessToken(id string, pairID string) (string, error) //return access, err
 	GenerateRefreshToken() (string, string, error)                //refresh, refreshHash, err
 	GeneratePairID() (string, error)
-	CheckPair(pair TokenPair) error
-	ValidateAccessToken(access string) (string, error)
+	ValidateAccessToken(access string) (string, string, error) //userID, pairID, err
 }
 
 type PasswordHasher interface {
