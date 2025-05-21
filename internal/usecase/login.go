4ebfada991567fe9ee97a79e85eee2e7f3b9bd92
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-21 14:54:43 +0300

    refactor: упрощена обработка ошибок, добавлен интерфейс
    новый интерфейс для проверки пароля, некоторые ошибки обрабатываю более компактно, возвращаю пустую структуру вместо указателя

--- internal/usecase/login.go
+++ internal/usecase/login.go
@@ -3,46 +3,44 @@ package usecase
 import (
 	"context"
 	"github.com/FooxyS/auth-service/internal/domain"
-	"github.com/FooxyS/auth-service/pkg/apperrors"
 )
 
 type LoginUseCase struct {
 	UserRepo    domain.UserRepository
 	SessionRepo domain.SessionRepository
 	tokens      domain.TokenService
+	hasher      domain.PasswordHasher
 }
 
-func (uc *LoginUseCase) Execute(ctx context.Context, email, password, ip, agent string) (*domain.TokenPair, error) {
+func (uc *LoginUseCase) Execute(ctx context.Context, email, password, ip, agent string) (domain.TokenPair, error) {
 	foundUser, errFindByEmail := uc.UserRepo.FindByEmail(ctx, email)
 	if errFindByEmail != nil {
-		return nil, errFindByEmail
+		return domain.TokenPair{}, errFindByEmail
 	}
-	rightPass := foundUser.CheckPassword(password)
-	if !rightPass {
-		return nil, apperrors.ErrPasswordMismatch
+
+	if err := uc.hasher.Compare(foundUser.PasswordHash, password); err != nil {
+		return domain.TokenPair{}, err
 	}
 
 	pairID, errGeneratePairID := uc.tokens.GeneratePairID()
 	if errGeneratePairID != nil {
-		return nil, errGeneratePairID
+		return domain.TokenPair{}, errGeneratePairID
 	}
 
 	access, errGenerateAccess := uc.tokens.GenerateAccessToken(foundUser.UserID, pairID)
 	if errGenerateAccess != nil {
-		return nil, errGenerateAccess
+		return domain.TokenPair{}, errGenerateAccess
 	}
 	refresh, refreshHash, errGenerateRefresh := uc.tokens.GenerateRefreshToken()
 	if errGenerateRefresh != nil {
-		return nil, errGenerateRefresh
+		return domain.TokenPair{}, errGenerateRefresh
 	}
-
-	pair := &domain.TokenPair{access, refresh}
 
 	session := domain.Session{UserID: foundUser.UserID, IPAddress: ip, RefreshHash: refreshHash, PairID: pairID, UserAgent: agent}
 
-	errSave := uc.SessionRepo.Save(ctx, session)
-	if errSave != nil {
-		return nil, errSave
+	if err := uc.SessionRepo.Save(ctx, session); err != nil {
+		return domain.TokenPair{}, err
 	}
-	return pair, nil
+
+	return domain.TokenPair{AccessToken: access, RefreshToken: refresh}, nil
 }
