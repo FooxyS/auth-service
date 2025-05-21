da6cb7e2c5bb8abb160e7f84feb1a0732df1e6ed
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-21 17:03:45 +0300

    feat: новые ошибки
    ошибки для RefreshUseCase

--- pkg/apperrors/errors.go
+++ pkg/apperrors/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrUserExists       = errors.New("user with this email already exists")
 	ErrPasswordMismatch = errors.New("password mismatch")
+	ErrIPMismatch       = errors.New("IP mismatch")
+	ErrAgentMismatch    = errors.New("agent mismatch, session was deleted")
 )
