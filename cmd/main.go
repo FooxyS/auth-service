66526d67a246432303cf277b148fa9fca265ad59
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 19:16:07 +0300

    created file

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/FooxyS/auth-service/router"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
-)
-
 func main() {
-	//подключение к БД
-	errLoadEnv := godotenv.Load()
-	if errLoadEnv != nil {
-		log.Fatalf("error with loading env file: %v\n", errLoadEnv)
-	}
 
-	dburl := os.Getenv("DATABASE_URL")
-
-	pgpool, errConnPostgres := pgxpool.New(context.Background(), dburl)
-	if errConnPostgres != nil {
-		log.Fatalf("error with connection to postgres: %v\n", errConnPostgres)
-	}
-
-	//запуск сервера
-	router := router.SetupRouter(pgpool)
-	errStart := http.ListenAndServe("localhost:8080", router)
-	if errStart != nil {
-		log.Fatalf("error with starting the server: %v\n", errStart)
-	}
 }
