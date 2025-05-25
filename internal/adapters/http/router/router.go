515e9e9727c770e35d42669b07fb5a347ee2aac6
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 19:06:13 +0300

    fix: аргументы
    sessionRepo был подан два, добавил UserRepo, поменял названия на лаконичные

--- internal/adapters/http/router/router.go
+++ internal/adapters/http/router/router.go
@@ -1,12 +1,13 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/FooxyS/auth-service/internal/adapters/http/handler"
 	"github.com/FooxyS/auth-service/internal/domain"
-	"net/http"
 )
 
-func SetupRouter(repository domain.SessionRepository, sessionRepository domain.SessionRepository, service domain.TokenService, hasher domain.PasswordHasher) http.Handler {
+func SetupRouter(UserRepo domain.UserRepository, SessionRepo domain.SessionRepository, Tokens domain.TokenService, Hasher domain.PasswordHasher) http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/register", &handler.RegisterHandler{})
 	mux.Handle("/login", &handler.LoginHandler{})
