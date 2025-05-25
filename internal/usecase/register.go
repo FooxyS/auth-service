0b4b61c1c01283183e70a9d1b00c0c928cd6300f
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 17:14:25 +0300

    !refactor: новая логика
    переписал логику RegisterUseCase. Теперь функция принимает email, сырой пароль, сама генерирует хэш из пароля, создаёт новый userID, формирует экземпляр пользователя, отправляет на сохранение

--- internal/usecase/register.go
+++ internal/usecase/register.go
@@ -8,16 +8,34 @@ import (
 
 type RegisterUseCase struct {
 	UserRepo domain.UserRepository
+	Hasher   domain.PasswordHasher
 }
 
-func (ru RegisterUseCase) Execute(ctx context.Context, user domain.User) error {
-	exist, errExists := ru.UserRepo.Exists(ctx, user.Email)
+func (ru RegisterUseCase) Execute(ctx context.Context, email, password string) error {
+	exist, errExists := ru.UserRepo.Exists(ctx, email)
 	if errExists != nil {
 		return errExists
 	}
 	if exist {
 		return apperrors.ErrUserExists
 	}
+
+	passHash, errHash := ru.Hasher.Hash(password)
+	if errHash != nil {
+		return errHash
+	}
+
+	newUserID, errGenerateID := ru.UserRepo.GenerateUserID()
+	if errGenerateID != nil {
+		return errGenerateID
+	}
+
+	user := domain.User{
+		UserID:       newUserID,
+		Email:        email,
+		PasswordHash: string(passHash),
+	}
+
 	errSave := ru.UserRepo.Save(ctx, user)
 	if errSave != nil {
 		return errSave
