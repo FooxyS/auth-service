07db0207120e04760e1bb9393afee23f9f73847c
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 17:37:15 +0300

    feat: новая функция
    вспомогательная функция для отправки ответа в loginHandler

--- internal/adapters/http/handler/services.go
+++ internal/adapters/http/handler/services.go
@@ -17,3 +17,16 @@ func WriteJSON(w http.ResponseWriter, code int, message string) {
 		log.Printf("WriteJSON error: %v", err)
 	}
 }
+
+func WriteLoginJSON(w http.ResponseWriter, code int, access, refresh string) {
+	resp := LoginResponse{
+		Access:  access,
+		Refresh: refresh,
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Printf("WriteLoginJSON error: %v", err)
+	}
+}
