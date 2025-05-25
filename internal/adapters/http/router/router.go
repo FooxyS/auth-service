d025c82222f22243b1bd44d635e0c8ea162eb551
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 19:15:31 +0300

    refactor: dependency injection like
    добавил обработку эндпоинтов с возможностью заинжектить внешнюю инфраструктуру

--- internal/adapters/http/router/router.go
+++ internal/adapters/http/router/router.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/FooxyS/auth-service/internal/adapters/http/handler"
 	"github.com/FooxyS/auth-service/internal/domain"
+	"github.com/FooxyS/auth-service/internal/usecase"
 )
 
 func SetupRouter(UserRepo domain.UserRepository, SessionRepo domain.SessionRepository, Tokens domain.TokenService, Hasher domain.PasswordHasher) http.Handler {
 	mux := http.NewServeMux()
-	mux.Handle("/register", &handler.RegisterHandler{})
-	mux.Handle("/login", &handler.LoginHandler{})
-	mux.Handle("/logout", &handler.LogoutHandler{})
-	mux.Handle("/refresh", &handler.RefreshHandler{})
-	mux.Handle("/me", &handler.MeHandler{})
+	mux.Handle("/register", &handler.RegisterHandler{UseCase: usecase.RegisterUseCase{UserRepo: UserRepo, Hasher: Hasher}})
+	mux.Handle("/login", &handler.LoginHandler{UseCase: usecase.LoginUseCase{UserRepo: UserRepo, SessionRepo: SessionRepo, Tokens: Tokens, Hasher: Hasher}})
+	mux.Handle("/logout", &handler.LogoutHandler{UseCase: usecase.LogoutUseCase{SessionRepo: SessionRepo, Tokens: Tokens}})
+	mux.Handle("/refresh", &handler.RefreshHandler{UseCase: usecase.RefreshUseCase{Tokens: Tokens, SessionRepo: SessionRepo, Hasher: Hasher}})
+	mux.Handle("/me", &handler.MeHandler{UseCase: usecase.MeUseCase{Tokens: Tokens, UserRepo: UserRepo}})
 
 	return mux
 }
