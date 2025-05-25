efd29d20a90a0f360ad8e41ceaa297ef064932d3
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-25 18:49:32 +0300

    feat: новая функция
    WriteMeJSON для отправки ответа в MeHandler

--- internal/adapters/http/handler/services.go
+++ internal/adapters/http/handler/services.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/FooxyS/auth-service/internal/domain"
 )
 
 func WriteJSON(w http.ResponseWriter, code int, message string) {
@@ -21,6 +23,19 @@ func WriteJSON(w http.ResponseWriter, code int, message string) {
 func WriteLoginJSON(w http.ResponseWriter, code int, access string) {
 	resp := TokenResponse{
 		Access: access,
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Printf("WriteLoginJSON error: %v", err)
+	}
+}
+
+func WriteMeJSON(w http.ResponseWriter, code int, user domain.User) {
+	resp := MeResponse{
+		UserID: user.UserID,
+		Email:  user.Email,
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
